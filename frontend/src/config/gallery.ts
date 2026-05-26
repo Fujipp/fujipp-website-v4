37206aa3811c7d4c51ed411abb3e6eaa938b8d7d
Fujipp <149602746+Fujipp@users.noreply.github.com>
2026-05-26 21:50:15 +0700

    fix(frontend): populate gallery image source

--- frontend/src/config/gallery.ts
+++ frontend/src/config/gallery.ts
@@ -7,7 +7,7 @@ export interface GalleryImage {
 export const galleryImages = [
     { id: "gallery-01", src: "/images/gallery/FCB671C3-0EFF-48AF-97ED-8B8F57AAE6A3_1_105_c.jpeg", alt: "Gallery image 1" },
     { id: "gallery-02", src: "/images/gallery/F8194A2C-5191-4D68-8F58-05DC76CBD989_1_105_c.jpeg", alt: "Gallery image 2" },
-    { id: "gallery-03", alt: "Gallery image 3" },
+    { id: "gallery-03", src: "/images/gallery/IT D-Day-2026-Photo.jpg", alt: "Gallery image 3" },
     { id: "gallery-04", alt: "Gallery image 4" },
     { id: "gallery-05", alt: "Gallery image 5" },
     { id: "gallery-06", alt: "Gallery image 6" },
